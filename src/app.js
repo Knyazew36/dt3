98454b01dfdc544715379ae2a68e1a2b89792e5b
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-03-22 19:04:49 +0300

    #header

--- src/app.js
+++ src/app.js
@@ -106,10 +106,121 @@ $(window).on('click', event => {
 // select
 {
   $(() => {
-    $('.select__select').select2({
+    // const select = $('.select__select');
+    $('.select__select').each(function () {
+      const select = $(this);
+      const selectWrapper = select.closest('.select-wrapper');
+      const selectWrapperStyles = getComputedStyle(selectWrapper[0]);
+      const selectPlaceholder = $(this).data('select-placeholder')
 
-      minimumResultsForSearch: -1,
-      dropdownParent: '.select-wrapper',
-    })
+      if (selectWrapperStyles.position === 'static') {
+        selectWrapper.css('position', 'relative');
+      }
+
+      select.select2({
+        dropdownParent: selectWrapper,
+        selectOnClose: true,
+        minimumResultsForSearch: Infinity,
+        // placeholder: selectPlaceholder,
+      });
+
+      select.on('select2:open', (e) => {
+        selectWrapper.css('z-index', '100000');
+        const evt = "scroll.select2";
+        $(e.target).parents().off(evt);
+        $(window).off(evt);
+
+        const selectDropdown = selectWrapper.find('.select2-dropdown');
+
+        selectDropdown.hide();
+        const timeout = setTimeout(() => {
+          selectDropdown.slideDown({ duration: 500, });
+
+          clearTimeout(timeout);
+        }, 0);
+      });
+
+      select.on('select2:closing', event => {
+        event.preventDefault();
+
+        const selectDropdown = selectWrapper.find('.select2-dropdown');
+
+        const timeout = setTimeout(() => {
+          selectWrapper.css('z-index', '');
+
+          const select2 = selectWrapper.find('.select2');
+
+          select2.addClass('closing');
+          select2.removeClass('select2-container--open');
+          selectDropdown.slideUp(500, () => {
+            const timeout2 = setTimeout(() => {
+              select.select2('destroy');
+              select.select2({
+                dropdownParent: selectWrapper,
+                selectOnClose: true,
+                minimumResultsForSearch: Infinity,
+                placeholder: selectPlaceholder,
+              });
+              select.removeClass('closing');
+
+              selectWrapper.css('z-index', '');
+
+              clearTimeout(timeout2);
+            }, 300);
+          });
+          clearTimeout(timeout);
+        }, 0);
+      });
+    });
+  });
+}
+
+//menu
+
+$(() => {
+  const header = $('.header')
+  const headerMenuBtn = $('.header__menu-btn');
+  const headerMenu = $('.menu')
+  const headerMenuContainer = $('.header__menu-container ')
+  const overlay = $('.header__overlay')
+
+  overlay.on('click', () => {
+    header.toggleClass('menu-open')
   })
+
+
+  headerMenuBtn.on('click', () => {
+
+
+
+    if ($(window).width() > 1200) {
+      // headerMenu.slideToggle(300)
+      header.toggleClass('menu-open')
+    } else {
+      header.toggleClass('menu-open')
+      headerMenuContainer.toggleClass('header__menu-container-active')
+
+    }
+
+
+  })
+})
+
+// map
+{
+  var myMap;
+
+  // Дождёмся загрузки API и готовности DOM.
+  ymaps.ready(init);
+
+  function init () {
+      // Создание экземпляра карты и его привязка к контейнеру с
+      // заданным id ("map").
+      myMap = new ymaps.Map('map', {
+          // При инициализации карты обязательно нужно указать
+          // её центр и коэффициент масштабирования.
+          center:[55.76, 37.64], // Москва
+          zoom:10
+      });
+  }
 }
